42bfdbecbd178577501ed752dcb9fe3adb02ccb8
Parker Selbert <parker@sorentwo.com>
2016-08-02 10:34:25 -0500

    Prepare mix and CHANGELOG for publication

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+## v0.1.0 2016-08-02
+
+* Initial release! Supports a Memory and Redis adapter.
--- mix.exs
+++ mix.exs
@@ -1,13 +1,25 @@
 defmodule Flippant.Mixfile do
   use Mix.Project
 
+  @version "0.1.0"
+
   def project do
     [app: :flippant,
-     version: "0.1.0",
+     version: @version,
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps]
+
+     description: description,
+     package: package,
+
+     deps: deps,
+
+     name: "Flippant",
+     source_url: "https://github.com/sorentwo/flippant",
+     docs: [source_ref: "v#{@version}",
+            extras: ["README.md"],
+            main: "Flippant"]]
   end
 
   def application do
@@ -16,7 +28,21 @@ defmodule Flippant.Mixfile do
      mod: {Flippant, []}]
   end
 
+  defp description do
+    """
+    Fast feature toggling for applications, backed by Redis.
+    """
+  end
+
+  defp package do
+    [maintainers: ["Parker Selbert"],
+     licenses: ["MIT"],
+     links: %{"GitHub" => "https://github.com/sorentwo/flippant"},
+     files: ~w(lib mix.exs README.md CHANGELOG.md)]
+  end
+
   defp deps do
-    [{:redix, "~> 0.4"}]
+    [{:redix, "~> 0.4"},
+     {:ex_doc, ">= 0.0.0", only: :dev}]
   end
 end
