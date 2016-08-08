bd0929c57ad50ecbe51e19bc4f64ab1f2c52853f
Parker Selbert <parker@sorentwo.com>
2016-08-08 08:47:20 -0500

    Document and typespec the Adapter module

--- lib/flippant/adapter.ex
+++ lib/flippant/adapter.ex
@@ -1,4 +1,17 @@
 defmodule Flippant.Adapter do
+  @moduledoc """
+  An adapter is a module that stores rules. Adapters should be stateful and run
+  in their own process. The built in adapters are all GenServers, and therefor
+  implement the GenServer behaviour.
+
+  For a breakdown of the expected calls and casts see Flippant.RuleRegistry
+  """
+
+  @doc """
+  Retrieve the `pid` of the configured adapter process. It is expected that the
+  adapter has been started.
+  """
+  @spec adapter() :: pid
   def adapter do
     :flippant
     |> Application.get_env(:adapter)
