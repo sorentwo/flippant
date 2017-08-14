7c4c7272ed4972960866e3c43ddfdc3644631fcc
Parker Selbert <parker@sorentwo.com>
2017-08-14 07:22:03 -0500

    Replace deprecated filter_map with filter and map

--- lib/flippant/adapters/redis.ex
+++ lib/flippant/adapters/redis.ex
@@ -120,7 +120,8 @@ if Code.ensure_loaded?(Redix) do
       features =
         features
         |> Enum.zip(results)
-        |> Enum.filter_map(&(elem(&1, 1) == 1), &(elem(&1, 0)))
+        |> Enum.filter(& elem(&1, 1) == 1)
+        |> Enum.map(& elem(&1, 0))
 
       {:reply, features, conn}
     end
